b9728af1a4fc70dab8125f91bc16ee01d9997b3e
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-09-18 17:57:42 -0400

    feat: added cli arg parser for standalone

--- biosimulators_simularium/converters/io.py
+++ biosimulators_simularium/converters/io.py
@@ -1,84 +1,15 @@
-import os
-from enum import Enum
-from typing import Optional, Tuple, Dict, List, Union
-from abc import ABC, abstractmethod
-from smoldyn import Simulation
-import smoldyn
-import numpy as np
-import pandas as pd
-import zarr
-from smoldyn.biosimulators.combine import validate_variables
-from smoldyn import biosimulators as bioSim
-from smoldyn.biosimulators.data_model import SmoldynOutputFile
-from simulariumio.smoldyn.smoldyn_data import InputFileData, SmoldynData
-from simulariumio.smoldyn import SmoldynConverter, SmoldynData
-from simulariumio.filters import TranslateFilter
-from simulariumio import (
-    TrajectoryData,
-    CameraData,
-    TrajectoryConverter,
-    AgentData,
-    UnitData,
-    MetaData,
-    DisplayData,
-    DISPLAY_TYPE,
-    ModelMetaData,
-    BinaryWriter,
-    InputFileData,
-)
-from biosimulators_utils.sedml.data_model import Task, Model, ModelLanguage
-from biosimulators_utils.config import Config, get_config
-from biosimulators_utils.report.data_model import ReportFormat
-from biosimulators_utils.sedml.data_model import UniformTimeCourseSimulation, Variable, Task
-from biosimulators_utils.model_lang.smoldyn.utils import get_parameters_variables_outputs_for_simulation
+from typing import Optional
 from biosimulators_simularium.converters.data_model import CombineArchive, SmoldynDataConverter
 
-"""
-def generate_new_simularium_file(archive: Archive, simularium_fp: SimulariumFilePath) -> None:
-    converter = SmoldynDataConverter(
-        archive=archive,
-        simularium_fp=simularium_fp,
-    )
+
+__all__ = [
+    'generate_new_simularium_file',
+]
 
 
-    # NOW MAKE A TASK!
-
-    '''_, _, configuration = validate_model(model)
-
-    simulation = configuration[0]
-
-    simulation.runSim()'''
-
-
-    '''result, log = generator.run_simulation_from_archive(ECOLI_OMEX_DIRPATH)
-    print(result)'''
-
-    '''generator.run_simulation_from_smoldyn_file(
-        os.path.join(ECOLI_ARCHIVE_DIRPATH, 'model.txt')
-    )'''
-
-    '''result, log = generator.run_simulation_from_archive(
-        archive_fp='biosimulators_simularium/files/archives/custom.omex',
-    )'''
-
-    # converter = SmoldynDataConverter(OUTPUTS_DIRPATH)
-
-    # r0, r1, r2 = validate_model('biosimulators_simularium/files/archives/Andrews_ecoli_0523/model.txt')
-    # print(r2)
-
-    # input_file_data = converter.prepare_input_file_data(model_out)
-    # data_object = converter.prepare_smoldyn_data_for_conversion(file_data=input_file_data)
-
-    # trans = converter.translate_data(data_object, 100.)
-    # converter.convert_to_simularium(data_object, 'biosimulators_simularium/minE_Andrews')
-    
-"""
-
-
-def generate_new_simularium_file(archive_rootpath: str):
-    minE_archive = CombineArchive(rootpath=archive_rootpath)
+def generate_new_simularium_file(archive_rootpath: str, simularium_filename: Optional[str] = None) -> None:
+    minE_archive = CombineArchive(rootpath=archive_rootpath, simularium_filename=simularium_filename)
     converter = SmoldynDataConverter(archive=minE_archive)
-    print(converter.read_model_output_dataframe())
-    # converter.generate_simularium_file()
+    return converter.generate_simularium_file(simularium_filename=simularium_filename)
 
 
--- biosimulators_simularium/main.py
+++ biosimulators_simularium/main.py
@@ -1,17 +1,32 @@
 import os
-from typing import Dict
-import platform
-import subprocess as subproc
-from biosimulators_utils.model_lang.smoldyn.utils import get_parameters_variables_outputs_for_simulation
-from biosimulators_utils.config import Config
-from biosimulators_utils.report.data_model import ReportFormat
-from biosimulators_utils.sedml.data_model import UniformTimeCourseSimulation, Variable, Task
+import argparse
 from biosimulators_simularium.converters.data_model import CombineArchive, SmoldynDataConverter
 from biosimulators_simularium.converters.io import generate_new_simularium_file
 from biosimulators_simularium.utils.io import remove_file, remove_output_files, parse_platform
 
 
-def convert(
+def run(archive_rootpath: str, simularium_file_name: str, install_smoldyn=False) -> None:
+    if install_smoldyn:
+        parse_platform()
+    return generate_new_simularium_file(archive_rootpath=archive_rootpath, simularium_filename=simularium_file_name)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert a smoldyn output file to simularium format")
+    parser.add_argument("-archive_rootpath", required=True, help="Path to the archive root")
+    parser.add_argument("-simularium_filename", required=True, help="Name of the Simularium file")
+    parser.add_argument("-install_smoldyn", action="store_true", help="Flag to install smoldyn")
+
+    args = parser.parse_args()
+
+    run(
+        archive_rootpath=args.archive_rootpath,
+        simularium_file_name=args.simularium_filename,
+        install_smoldyn=args.install_smoldyn
+    )
+
+
+def test_run(
         archive_rootpath: str,
         archive_output_dirpath: str = None,
         new_simularium_fp: str = None,
@@ -31,15 +46,10 @@ def convert(
 
 
 archive_root = 'biosimulators_simularium/files/archives/Andrews_ecoli_0523'
-
-
-def test_convert(archive_rootpath=archive_root):
-    minE_archive = CombineArchive(rootpath=archive_rootpath, simularium_filename='MY_TEST_SIMULARIUM_OUTPUT')
-    converter = SmoldynDataConverter(archive=minE_archive)
-    converter.generate_simularium_file()
+simularium_fp = os.path.join(archive_root, 'MY_SIMULARIUM_TEST_OUTPUT')
 
 
 if __name__ == '__main__':
     # convert(archive_rootpath=archive_root, install_smoldyn=False)
-    # generate_new_simularium_file(archive_rootpath=archive_root)
-    test_convert(archive_root)
+    # generate_new_simularium_file(archive_rootpath=archive_root, simularium_filename=simularium_fp)
+    main()
