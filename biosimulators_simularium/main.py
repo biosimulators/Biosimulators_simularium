4195e0fbf051632fe4e6d473d1aeb3df3dbede66
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-09-19 09:04:45 -0400

    feat: streamlined cli entrypoint in setuptools and adjust Dockerfile

--- biosimulators_simularium/main.py
+++ biosimulators_simularium/main.py
@@ -25,7 +25,9 @@ def main():
         install_smoldyn=args.install_smoldyn
     )
 
+
 # NOW ADD A METHOD THAT ADDS THE SIMULARIUM FILE TO THE XML MANIFEST!
+
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(),
+    entry_points={
+            'console_scripts': [
+                'biosimulators-simularium = biosimulators_simularium.main:main',
+            ],
+    },
 )
