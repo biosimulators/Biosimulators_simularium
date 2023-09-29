c72b0e7e8cab1ca35ee9153824ae992caf783976
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-09-29 08:20:16 -0400

    feat: added zarr writer to io lib

--- biosimulators_simularium/io/data_model.py
+++ biosimulators_simularium/io/data_model.py
@@ -1,5 +1,7 @@
-from typing import List
+import os
+from typing import List, Optional, Tuple, Union
 from abc import ABC, abstractmethod
+import zarr
 
 
 class FileWriter(ABC):
@@ -15,7 +17,7 @@ class FileWriter(ABC):
         pass
 
     @abstractmethod
-    def read(self, fp: str, data):
+    def read(self, fp: str):
         pass
 
     @abstractmethod
@@ -24,6 +26,29 @@ class FileWriter(ABC):
 
 
 class BinaryFileWriter(FileWriter):
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
+
+    def _header(self, **header_items):
+        pass
+
+    def _padding(self, n_pad: int):
+        pass
+
+    def read(self, fp: str):
+        with open(fp, 'wb') as f:
+            for datum in self.data:
+                f.write(datum.encode())
+
+    def write(self, fp: str, data: List):
+        with open(fp, 'rb') as f:
+            content = f.read().decode()
+            val = list(content)
+            return val
+
+
+class ZarrWriter(FileWriter):
     def __init__(self):
         super().__init__()
 
@@ -33,13 +58,20 @@ class BinaryFileWriter(FileWriter):
     def _padding(self, n_pad: int):
         pass
 
-    def read(self, fp: str, data: List):
-        with open(fp, 'wb') as f:
-            for datum in data:
-                f.write(datum.encode())
+    def read(self, fp: str):
+        return zarr.open(fp, mode='r') if os.path.exists(fp) else None
 
-    def write(self, fp: str, data: List):
-        with open(fp, 'rb') as f:
-            content = f.read().decode()
-            val = list(content)
-            return val
+    def write(
+            self,
+            fp: str,
+            data,
+            chunks: Tuple[int, int],
+            shape: Union[Tuple[int, int], Tuple[int, int, int]],
+            dtype: str,
+            open_mode: str = 'w'):
+        return zarr.open(fp, mode=open_mode, shape=shape, chunks=chunks, dtype=dtype)
+
+    @staticmethod
+    def get_slice(arr: zarr.Array, xA: int, xZ: int, yA: int, yZ: int):
+        return arr[xA:xZ, yA:yZ]
+
