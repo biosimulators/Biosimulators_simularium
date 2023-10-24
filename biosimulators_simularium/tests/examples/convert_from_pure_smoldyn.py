3216f93c9ac0a2702d018008f2621490f197892f
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-10-24 08:03:46 -0400

    fix: updated methods to include other files parsing

---
+++ biosimulators_simularium/tests/examples/convert_from_pure_smoldyn.py
@@ -0,0 +1,32 @@
+import os
+from typing import *
+import smoldyn
+from biosimulators_utils.model_lang.smoldyn.validation import validate_model
+from biosimulators_simularium.archives.data_model import SmoldynCombineArchive
+from biosimulators_simularium.converters.data_model import SmoldynDataConverter
+
+
+crowding_archive_path = os.path.join('biosimulators_simularium', 'tests', 'fixtures', 'archives', 'crowding4')
+
+archive = SmoldynCombineArchive(rootpath=crowding_archive_path, simularium_filename='crowding4')
+
+validation = validate_model(archive.model_path)
+
+
+def parse_simulation(validation: Tuple):
+    simulation = []
+    for item in validation:
+        if len(item):
+            for thing in item:
+                if isinstance(thing, smoldyn.Simulation):
+                    return thing.runSim()
+
+
+converter = SmoldynDataConverter(archive)
+
+agents = [
+    ('red(up)', 0.2),
+    ('green(up)', 0.2),
+]
+
+converter.generate_simularium_file(agents=agents, io_format='JSON')
