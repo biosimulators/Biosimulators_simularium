53cabea3c1077f2e99ec32b8622588693c44ff93
alexPatrie <alexanderpatrie@gmail.com>
2023-12-26 16:41:09 -0500

    added file

--- biosimulators_simularium/tests/test_convert_minE.py
+++ biosimulators_simularium/tests/test_convert_minE.py
@@ -5,10 +5,7 @@ import os
 from biosimulators_simularium.convert import generate_output_data_object
 
 
-def convert_minE(model_fp: str, display_data_dict):
+def test_convert_minE(model_fp: str, display_data_dict):
     """TODO: Create display data dict for each mol in the simulation."""
 
-    data_obj = generate_output_data_object(
-        file_data=model_fp,
-
-    )
+    model_fp =
