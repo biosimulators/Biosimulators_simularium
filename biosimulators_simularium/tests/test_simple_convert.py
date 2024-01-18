b4e1756c1ed971ab30f72442745a89c2094a12e9
alexPatrie <alexanderpatrie@gmail.com>
2024-01-18 11:43:56 -0500

    removed pyvista as dep

--- biosimulators_simularium/tests/test_simple_convert.py
+++ biosimulators_simularium/tests/test_simple_convert.py
@@ -1,4 +1,10 @@
 import os
 from biosimulators_simularium import generate_simularium_file
 
-archive_root = 'biosimulators_simularium/tests/fixtures/archives/MinE'
+
+def test_convert():
+    archive_root = 'biosimulators_simularium/tests/fixtures/archives/MinE'
+    simularium_fn = 'simulation'
+
+
+
