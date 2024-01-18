4a426f6c53ea5ce3d783982192d532dcc8517e54
alexPatrie <alexanderpatrie@gmail.com>
2024-01-18 18:51:13 -0500

    updated version and release script

--- release.sh
+++ release.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# !/bin/zsh
+#!/bin/zsh
 
 # Create and publish a new version by creating and pushing a git tag for
 # the version and publishing the version to PyPI. Also perform some
@@ -8,10 +7,15 @@
 
 set -e
 
+use_poetry="$1"
 version=$(grep "__version__" setup.py | awk -F\' '{print $2}')
 
 # Check version is valid
-setup_py_version="$(python setup.py --version)"
+if [ "$use_poetry" != 1 ]; then
+  setup_py_version="$(python setup.py --version)"
+else
+  setup_py_version="$(poetry run python3 setup.py --version)"
+fi
 if [ "$setup_py_version" != "$version" ]; then
     echo "setup.py has version $setup_py_version, not $version."
     echo "Aborting."
@@ -22,7 +26,7 @@ fi
 if [ ! -z "$(git status --untracked-files=no --porcelain)" ]; then
     echo "You have changes that have yet to be committed."
     echo "Aborting PyPI upload and attempting to commit your changes."
-    scripts/commit.sh
+    ./commit.sh
 fi
 
 # Check that we are on main
@@ -38,10 +42,20 @@ git tag -m "Version v$version" "v$version"
 git push --tags
 
 # Create and publish package
-python setup.py sdist bdist_wheel
-twine check dist/*
-twine upload dist/*
+if [ "$use_poetry" != 1 ]; then
+  echo "Not using poetry."
+  python setup.py sdist bdist_wheel
+  twine check dist/*
+  twine upload dist/*
+  rm -r dist && rm -r biosimulators_simularium.egg-info && rm -r build
+else
+  echo "Using poetry"
+  poetry run python3 setup.py sdist
+  twine check dist/*
+  twine upload dist/*
+  rm -r dist && rm -r biosimulators_simularium.egg-info
+fi
+
 # twine upload --repository biosimulators-simularium
-rm -r dist && rm -r build && rm -r biosimulators_simularium.egg-info
 
 echo "Version v$version has been published on PyPI and has a git tag."
