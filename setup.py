cf0eb782119f70f6d64152f4dee809165134a989
alexPatrie <alexanderpatrie@gmail.com>
2024-01-18 18:51:57 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 # noinspection PyProtectedMember
 
-__version__ = '0.5.12'
+__version__ = '0.5.13'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
