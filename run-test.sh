e0b2d50959bbb896c72a2417293e35324a44bb3e
alexPatrie <alexanderpatrie@gmail.com>
2024-01-18 16:26:52 -0500

    udpated namespace

--- run-test.sh
+++ run-test.sh
@@ -1,13 +1,7 @@
-#!/bin/bash 
-
-use_poetry="$1"
+#!/bin/bash
 
 test_file="$2"
 
-if [ "${use_poetry}"=="0" ]; then
-    conda activate biosimularium
-    python3 biosimulators_simularium/tests/"${test_file}".py
-else
-    poetry run python3 biosimulators_simularium/tests/"${test_file}".py
-fi
+poetry run python3 biosimulators_simularium/tests/"${test_file}".py
+
  
