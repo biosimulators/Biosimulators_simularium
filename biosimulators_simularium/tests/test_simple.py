fdc031994bf5e899ab5cde148674481270300b19
alexPatrie <alexanderpatrie@gmail.com>
2024-01-18 18:19:20 -0500

    adjusted fine grained test

--- biosimulators_simularium/tests/test_simple.py
+++ biosimulators_simularium/tests/test_simple.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 from simulariumio.smoldyn.smoldyn_data import SmoldynData
 from biosimulators_simularium import generate_simularium_file
-from biosimulators_simularium.io import get_model_fp
+from biosimulators_simularium.io import get_model_fp, get_archive_files
 from biosimulators_simularium.convert import generate_output_data_object, display_data_dict_agent_major, get_species_names_from_model_file, display_data_dict_from_archive_model
 from biosimulators_simularium.simulation_data import generate_agent_params
 from biosimulators_simularium.exec import generate_simularium_file
@@ -30,6 +30,7 @@ def test_tempdir_convert():
     is_json = True
     out_dir = tempfile.mkdtemp()
     generate_simularium_file(working_dir=archive_root, simularium_filename=simularium_name, output_dir=out_dir)
+    print(f'Output dir: {get_archive_files(out_dir)}')
     assert_clause(os.path.exists(os.path.join(out_dir, simularium_name + '.simularium')))
 
 
