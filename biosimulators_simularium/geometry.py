507b6145cebc4042c9c1fc665c05edb82f4932d0
alexPatrie <alexanderpatrie@gmail.com>
2024-01-26 11:24:34 -0500

    updated geometry setter and model files

--- biosimulators_simularium/geometry.py
+++ biosimulators_simularium/geometry.py
@@ -1,9 +1,13 @@
 import os
+from typing import *
 from dataclasses import dataclass
 import pyvista as pv
-from typing import *
+import vtk
 
 
+
+
+# Pyvista Content
 @dataclass
 class Capsule:
     cyl: pv.Cylinder
@@ -11,7 +15,7 @@ class Capsule:
 
 
 def construct_capsule(R: float = 0.5, L_PARAM: int = 2) -> pv.PolyData:
-    """Construct a capsule shape out of a cylinder and two spheres.
+    """Construct a capsule shape out of a cylinder and two spheres with pyvista
 
     TODO: Encode json and derive the parameters automatically from the simulation instance.
     """
@@ -21,4 +25,6 @@ def construct_capsule(R: float = 0.5, L_PARAM: int = 2) -> pv.PolyData:
     hemisphere1 = pv.Sphere(radius=R, center=(L_PARAM, 0, 0))
     hemisphere2 = pv.Sphere(radius=R, center=(-L_PARAM, 0, 0))
     return cylinder.merge(hemisphere1).merge(hemisphere2)
+
+
 
